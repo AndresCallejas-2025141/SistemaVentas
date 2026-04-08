7a18eac30a65b4f2bcd883b70447f0d653f41c3c
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-04-08 11:19:10 -0600

    add: Se agrego el HomeController

--- SistemaVentas/SistemaVentas/SistemaVentas/src/main/java/com/ecallejas/SistemaVentas/Controller/HomeController.java
+++ SistemaVentas/SistemaVentas/SistemaVentas/src/main/java/com/ecallejas/SistemaVentas/Controller/HomeController.java
@@ -1,4 +1,38 @@
 package com.ecallejas.SistemaVentas.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping({"/", "/inicio"})
+    public String home() {
+        return "home"; // home.html
+    }
+
+    @GetMapping("/ventas")
+    public String ventas() {
+        return "ventas"; // ventas.html
+    }
+
+    @GetMapping("/detalleventas")
+    public String detalleVentas() {
+        return "detalleventas"; // detalleventas.html
+    }
+
+    @GetMapping("/productos")
+    public String productos() {
+        return "productos"; // productos.html
+    }
+
+    @GetMapping("/clientes")
+    public String clientes() {
+        return "clientes"; // clientes.html
+    }
+
+    @GetMapping("/usuarios")
+    public String usuarios() {
+        return "usuarios"; // usuarios.html
+    }
 }
