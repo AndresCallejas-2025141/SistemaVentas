87fe96a931ceb0edac84de99fe2f4cfb57efea77
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-04-14 11:27:54 -0600

    refactor: Se cambiaron los datos

--- SistemaVentas/SistemaVentas/SistemaVentas/src/main/java/com/ecallejas/SistemaVentas/Service/UsuariosService.java
+++ SistemaVentas/SistemaVentas/SistemaVentas/src/main/java/com/ecallejas/SistemaVentas/Service/UsuariosService.java
@@ -1,12 +1,16 @@
 package com.ecallejas.SistemaVentas.Service;
+
 import com.ecallejas.SistemaVentas.Entity.Usuarios;
-import org.springframework.stereotype.Service;
+
 import java.util.List;
-@Service
+
 public interface UsuariosService {
-    List<Usuarios> getAllUsuarios();
-    Usuarios getUsuariosById(Integer codigoUsuario);
-    Usuarios saveUsuarios(Usuarios usuarios) throws RuntimeException;
-    Usuarios updateUsuarios(Integer codigoUsuario, Usuarios usuarios);
-    void deleteUsuarios(Integer codigoUsuario);
+
+    Usuarios registrar(String username, String password);
+
+    Usuarios login(String username, String password);
+
+    List<Usuarios> listar();
+
+    void eliminar(Integer id);
 }
