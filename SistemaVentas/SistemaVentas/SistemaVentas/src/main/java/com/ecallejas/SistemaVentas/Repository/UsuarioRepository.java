ae292d48b267882e675359665f2727db06b3b4a1
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-04-14 11:27:10 -0600

    test: Se agrego findByUsername para buscar registro de la DB

--- SistemaVentas/SistemaVentas/SistemaVentas/src/main/java/com/ecallejas/SistemaVentas/Repository/UsuarioRepository.java
+++ SistemaVentas/SistemaVentas/SistemaVentas/src/main/java/com/ecallejas/SistemaVentas/Repository/UsuarioRepository.java
@@ -1,9 +1,9 @@
 package com.ecallejas.SistemaVentas.Repository;
+
 import com.ecallejas.SistemaVentas.Entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-@Component
-public interface UsuarioRepository  extends JpaRepository<Usuarios,Integer>{
 
+public interface UsuarioRepository extends JpaRepository<Usuarios, Integer> {
+
+    Usuarios findByUsername(String username);
 }
